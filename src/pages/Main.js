5196889447299ee9b8407fb319db92ec1ad5ac45
ej-rarus <lpl2001@naver.com>
2024-11-04 17:11:54 +0900

    backgroundcolor test

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,6 @@
 export default function Main() {
   return (
-    <div className="page-container">
+    <div className="page-container main-test">
       <div className="">
         Main 입니다.
       </div>
