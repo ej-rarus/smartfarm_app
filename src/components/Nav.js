bd7a5c8bd1009fd602b2b31baaa4f13e54829299
ej-rarus <lpl2001@naver.com>
2024-11-04 16:01:19 +0900

    Navbar sign in 버튼 추가

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faRightToBracket  } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
 
 function Nav({ menuVisible, setMenuVisible }) {
@@ -17,7 +17,10 @@ function Nav({ menuVisible, setMenuVisible }) {
           </div>
         </div>
         <div className="nav-btn-container">
-          <div id="burger-menu-btn" onClick={toggleMenu}>
+          <div id="sign-in-btn" className="nav-btn" onClick={()=>navigate("/login")}>
+            <FontAwesomeIcon icon={faRightToBracket} size="2x" />
+          </div>
+          <div id="burger-menu-btn" className="nav-btn" onClick={toggleMenu}>
             <FontAwesomeIcon icon={faBars} size="2x" /> {/* 버거 아이콘 */}
           </div>
         </div>
